f67a9b2c7609f7603f873ee367ad66f596bdc9da
Jackie <jiajie.wu96@gmail.com>
2018-10-22 16:14:36 -0700

    HighScore data model added

---
+++ app/src/main/java/com/example/wu/jackie/braintrainer/model/HighScoreModel.java
@@ -0,0 +1,7 @@
+package com.example.wu.jackie.braintrainer.model;
+
+public interface HighScoreModel {
+    String getPlayerName();
+    int getPlayerScore();
+    int getPercentQuestionsCorrect();
+}
